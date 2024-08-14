59d46d951a791a96e76353d2f30c461e65944171
Yarik <ya.breyner@gmail.com>
2024-08-15 00:35:34 +0300

    config + rebase

---
+++ VapeShop.Data/Context/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VapeShop.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options)
+        {
+            
+        }
+
+
+    }
+}
--- VapeShop/wwwroot/js/site.js
+++ VapeShop/wwwroot/js/site.js
@@ -1,4 +1,51 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-// Write your JavaScript code.
+if (ScrollTrigger.isTouch !== 1) {
+
+	ScrollSmoother.create({
+		wrapper: '.wrapper',
+		content: '.content',
+		smooth: 1.5,
+		effects: true
+	})
+
+	gsap.fromTo('.hero-section', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.hero-section',
+			start: 'center',
+			end: '820',
+			scrub: true
+		}
+	})
+
+	let itemsL = gsap.utils.toArray('.gallery__left .gallery__item')
+
+	itemsL.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: -50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-850',
+				end: '-100',
+				scrub: true
+			}
+		})
+	})
+
+	let itemsR = gsap.utils.toArray('.gallery__right .gallery__item')
+
+	itemsR.forEach(item => {
+		gsap.fromTo(item, { opacity: 0, x: 50 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: '-750',
+				end: 'top',
+				scrub: true
+			}
+		})
+	})
+
+}
+
